18e011fe38b732107f4a937930d9177b91f869ce
lcc <lcc0739@163.com>
2020-10-12 17:42:52 +0800

    First commit

---
+++ cloud-provider-payment8001/src/main/java/com/lcc/springcloud/service/PaymentService.java
@@ -0,0 +1,15 @@
+package com.lcc.springcloud.service;
+
+import com.lcc.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 10:40
+ */
+public interface PaymentService
+{
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
